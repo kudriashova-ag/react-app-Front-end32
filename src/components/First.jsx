ebf033cf430a0359d70d987a86118832f555abb9
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2024-04-09 21:17:27 +0300

    first commit

---
+++ src/components/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const First = () => {
+    return (
+        <>
+            <h1>First component</h1>
+            <p>Lorem, ipsum.</p>
+        </>
+    );
+}
+
+export default First;
