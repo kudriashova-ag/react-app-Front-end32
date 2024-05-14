917c66a8a84f4aa1f65141b957ab2a31d9fb9f9a
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2024-05-14 20:21:00 +0300

    useSerachParams

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import { NavLink, Outlet, useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Outlet, useLoaderData, useSearchParams } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
+  const [search, setSearch] = useSearchParams();
+  const [textSearch, setTextSearch] = useState(search.get("q"));
+
+  const searchHandler = (e) => {
+    setTextSearch(e.target.value);
+    setSearch({ q: e.target.value });
+  };
+
+  const filterUsers = (user) => {
+    //  return user.name.includes(textSearch);
+    // return user.name.toLowerCase().startsWith(textSearch.toLowerCase());
+    return new RegExp("^" + textSearch, "i").test(user.name);
+   }
 
   return (
     <div>
+      <input type="text" value={textSearch} onChange={searchHandler} />
+
       <div>
-        {users.map((user) => (
+        {users.filter(filterUsers).map((user) => (
           <div key={user.id}>
             <NavLink to={`/users/${user.id}`}>{user.name}</NavLink>
           </div>
